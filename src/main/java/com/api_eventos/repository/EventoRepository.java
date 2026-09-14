fe6f1bb4c6542af572b6c444128d5e3b6114ad59
Leonardo <leonardoherrerasabbatini@gmail.com>
2026-09-14 09:30:17 -0300

    Create EventoRepository.java

---
+++ src/main/java/com/api_eventos/repository/EventoRepository.java
@@ -0,0 +1,9 @@
+package com.api_eventos.repository;
+
+import com.api_eventos.model.Evento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EventoRepository extends JpaRepository<Evento, Long> {
+}
