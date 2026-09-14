c33c053b6fae176c8306a3030e3c05b038e55eb3
Luiza <luiza12pinheiro@gmail.com>
2026-09-14 10:49:28 -0300

    criando arquivo participante service

--- src/main/java/com/api_eventos/repository/ParticipanteRepository.java
+++ src/main/java/com/api_eventos/repository/ParticipanteRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ParticipanteRepository extends JpaRepository<Long, Participante> {
+public interface ParticipanteRepository extends JpaRepository<Participante, Long> {
+    boolean existsByEmail(String email);
 
 }
