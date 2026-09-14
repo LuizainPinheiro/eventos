224d5531778c69a3c85319158c7ac8850bac5e51
Leonardo <leonardoherrerasabbatini@gmail.com>
2026-09-14 11:31:11 -0300

    Add Inscricao entity with JPA annotations

--- src/main/java/com/api_eventos/model/Inscricao.java
+++ src/main/java/com/api_eventos/model/Inscricao.java
@@ -1,1 +1,32 @@
+package com.api_eventos.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Inscricao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDate dataInscricao;
+
+    @ManyToOne
+    @JoinColumn()
+    private Evento evento;
+
+    @ManyToOne
+    private Participante participante;
+}
 
