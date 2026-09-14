eec1e890720f74ac9d8f0064f48ca224437b7222
Luiza <luiza12pinheiro@gmail.com>
2026-09-14 09:19:34 -0300

    criando a model

---
+++ src/main/java/com/api_eventos/model/Participante.java
@@ -0,0 +1,29 @@
+package com.api_eventos.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Participante {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String email;
+
+    @OneToMany(mappedBy = "inscricao")
+    private Inscricao inscricao;
+}
+
