ffdb151da14e53ae380a95fcaae72d6e83960bd2
Luiza <luiza12pinheiro@gmail.com>
2026-09-15 10:16:29 -0300

    listar participantes de um evento

--- src/main/java/com/api_eventos/repository/InscricaoRepository.java
+++ src/main/java/com/api_eventos/repository/InscricaoRepository.java
@@ -5,9 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Repository
 public interface InscricaoRepository extends JpaRepository<Inscricao, Long> {
+
+    List<Inscricao> findByEventoId(Long eventoId);
 
     boolean existsByDataInscricaoAndParticipanteIdAndEventoId(
             LocalDate dataInscricao,
